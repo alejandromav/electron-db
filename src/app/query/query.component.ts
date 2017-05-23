7a15b1b2ae586689aa602f72cd0baf8c7985564b
Alejandro Martin <alejandromav@ext.inditex.com>
2017-05-23 17:13:37 +0200

    fix

--- src/app/query/query.component.ts
+++ src/app/query/query.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+declare var CodeFlask: any;
 
 @Component({
     selector: 'app-query',
     templateUrl: './query.component.html',
     styleUrls: ['./query.component.scss']
 })
-export class QueryComponent {
+export class QueryComponent implements OnInit {
 
     constructor(
         private router: Router
-    ) { }
+    ) { 
+    }
+
+    ngOnInit () {
+        var flask = new CodeFlask;
+        flask.run('#my-code-wrapper', { language: 'sql', lineNumbers: true })
+    }
 
     navigateTo(path) {
         this.router.navigate([path]);
